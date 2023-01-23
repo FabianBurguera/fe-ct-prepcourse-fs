b111d443cf41bf6026be02af122e806336d1856c
FabianBurguera <fabi.burguera@hotmail.com>
2023-01-23 18:37:26 -0300

    Homework hecha

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -139,7 +139,7 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Tu código:
    var contador = 0 ;
    for (var i=0 ;i<objetoUsuario.posts.length ; i++ ){
-     contador = contador + this.posts[i].likes ;
+     contador = contador + this.post[i].likes ;
 }
 return contador ;
 }
