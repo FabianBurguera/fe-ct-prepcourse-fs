ac9908bd8a9af96dd6ab16f19e7e5090d1f1c25b
FabianBurguera <fabi.burguera@hotmail.com>
2023-02-04 14:01:10 -0300

    haciendo hw

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,10 +6,19 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+//*recibo un objeto, ejemplo objeto = { a:1,b:2,c:3}
+// declaro un array 
+let ArrArr = Object.entries(objeto) ;
+
 }
 
+
+   
+
+
 function numberOfCharacters(string) {
-   // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
+   // La función recibe un string. Debes recorrerlo y 
+   //retornar un objeto donde cada propiedad es una de las
    // letras del string, y su valor es la cantidad de veces que se repite en el string.
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
